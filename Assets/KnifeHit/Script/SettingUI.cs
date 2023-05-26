5a4f14883722ad7414424c07e5c35534c3385697
Nguyen Chinh <trongchinhnguyen1@gmail.com>
2023-05-26 20:41:52 +0700

    cc
    ccc

---
+++ Assets/KnifeHit/Script/SettingUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour {
+
+	[Header("Setting View")]
+	public  Toggle soundToggle;
+	public  GameObject UIParent;
+    public Text removeAdPriceText;
+	public static SettingUI intance;
+
+	void Awake()
+	{
+		if (intance == null) 
+		{
+			intance = this;
+		}
+	}
+
+	void Start()
+	{
+		soundToggle.onValueChanged.RemoveAllListeners ();
+		updateUI ();
+		soundToggle.onValueChanged.AddListener ((arg0) =>{ 
+			GameManager.Sound=arg0;
+			if(arg0)
+				SoundManager.instance.PlaybtnSfx ();
+		} );
+	}
+
+    public void showUI()
+	{
+		UIParent.SetActive (true);
+	}
+
+	public void updateUI()
+	{
+		soundToggle.isOn = GameManager.Sound;
+	}
+
+}
